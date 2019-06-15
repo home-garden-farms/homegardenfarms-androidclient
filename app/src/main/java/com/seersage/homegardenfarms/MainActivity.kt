b3682f3efcaa4bf6e3ddd60fc21bc5b6ac571c77
mark <markesargent@gmail.com>
2019-06-14 17:12:37 -0700

    Some packages added, landing fragment created

--- app/src/main/java/com/seersage/homegardenfarms/MainActivity.kt
+++ app/src/main/java/com/seersage/homegardenfarms/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.seersage.homegardenfarms
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.seersage.homegardenfarms.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
